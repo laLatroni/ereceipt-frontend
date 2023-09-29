ec873c2484abcbf121295038043cd754551076ad
paulpdoa <polopdoandres@gmail.com>
2023-09-29 13:42:39 +0800

    done

--- src/components/DateFormat.jsx
+++ src/components/DateFormat.jsx
@@ -1,7 +1,7 @@
 const DateFormat = ({ date }) => {
 
     const monthList = ['January','February','March','April','May','June','July','August','September','October','November','December'];
-    const formatDate = `${date.split('-')[2]} ${monthList[Number(date.split('-')[1] - 1)]} ${date.split('-')[0]}`;
+    const formatDate = `${date?.split('-')[2]} ${monthList[Number(date?.split('-')[1] - 1)]} ${date?.split('-')[0]}`;
 
     return formatDate
 }
