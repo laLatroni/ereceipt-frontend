f7c098cf6ec1d55622a7c35f512f9599f3fa0fbc
laLatroni <ivofspadesunique@gmail.com>
2023-10-16 14:13:00 +0800

    FRONT END DEPLOYMENT

--- src/baseUrl.js
+++ src/baseUrl.js
@@ -2,6 +2,6 @@ export const baseUrl = () => {
     if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
         return 'http://localhost:8080/api/v1';
     } else {
-        return 'production link';
+        return 'https://please-gumana-kana.onrender.com/api/v1';
     }
 }
