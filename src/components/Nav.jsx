69dba5afc3e2dea401bb0e2ec64a4ab53b6dedd4
paulpdoa <polopdoandres@gmail.com>
2023-11-30 14:31:17 +0800

    added with credentials on logout

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -6,7 +6,7 @@ const Nav = () => {
 
     const logout = async () => {
         try {
-            const data = await axios.get(`http://localhost:8080/api/v1/logout`);
+            const data = await axios.get(`http://localhost:8080/api/v1/logout`,{ withCredentials: true });
             console.log(data);
         } catch(err) {
             console.log(err)
